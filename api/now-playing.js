121a8eb5dcaea2be58780eb4a3471ff6f83036c7
Sourav Goswami <souravgoswami2005@gmail.com>
2026-06-25 13:07:58 +0530

    fix: handle spotify premium forbidden error gracefully on now-playing endpoint

--- api/now-playing.js
+++ api/now-playing.js
@@ -40,6 +40,9 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
 
+  try {
+    const { access_token } = await getAccessToken();
+
     // Try currently playing first
     const npRes = await fetch(NOW_PLAYING_ENDPOINT, {
       headers: { Authorization: `Bearer ${access_token}` },
